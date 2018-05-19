b9218792e1064dfa0568d2d4909baf74b05ea02b
Splinter Suidman <splinter@mannenopdemaan.nl>
2018-05-19 19:11:03 +0200

    Update example.

--- example.sh
+++ example.sh
@@ -1,6 +1,6 @@
 echo '
 Regular: The quick brown fox jumps over the lazy dog.
-         The quick brown fox jumps over the lazy dog.
+         THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
 Bold:    \033[1mThe five boxing wizards jump quickly.\033[0m
          \033[1mTHE FIVE BOXING WIZARDS JUMP QUICKLY.\033[0m
 Italic:  \033[3mHow vexingly quick daft zebras jump!\033[23m
